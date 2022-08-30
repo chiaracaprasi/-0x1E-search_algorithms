fb4040144cd871c0af3666dc1985a2b89717a7f0
Chiara Caprasi <91517809+chiaracaprasi@users.noreply.github.com>
2022-08-30 16:41:26 +1000

    update: add edge cases

--- 0-linear.c
+++ 0-linear.c
@@ -11,6 +11,9 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+	
+	if (array == NULL || size == 0)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
@@ -18,5 +21,5 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] == value)
 			return (i);
 	}
-		return (-1);
+	return (-1);
 }
