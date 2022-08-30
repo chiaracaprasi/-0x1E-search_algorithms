3cfb7404541ff1947e5e415d64d3bb1176e62b16
Karoline-S <karoline.amy@gmail.com>
2022-08-29 21:44:05 -0700

    docs - update header

--- search_algos.h
+++ search_algos.h
@@ -40,6 +40,7 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int size);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
